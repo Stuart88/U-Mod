b969e20fcd695c9de938be8caed92ea835a85a76
Stuart Aitken <s.aitken88@gmail.com>
2021-05-27 21:09:37 +0100

    Update software version

--- U-Mod.Shared/Constants/Constants.cs
+++ U-Mod.Shared/Constants/Constants.cs
@@ -6,7 +6,7 @@ namespace U_Mod.Shared.Constants
 {
     public static class Constants
     {
-        public const string InstallerVersion = "1.0.5";
+        public const string InstallerVersion = "1.0.6";
 
         public const string NexusAppSlug = "vortex";
 
